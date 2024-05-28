7855e5c1c57f962d369c7347954fccd29939453f
Flomo Kokro <fkokro@yahoo.com>
2024-05-28 14:25:54 -0400

    55 jump game

---
+++ leetcode/array_string/55JumpGame.py
@@ -0,0 +1,36 @@
+"""
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+"""
+import unittest
+
+def canJump(nums: list[int]) -> bool:
+    end = len(nums) - 1
+
+    for i in range(len(nums)-1, -1,-1):
+        if i + nums[i] >= end:
+            end = i
+            
+    return True if end == 0 else False
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        self.assertEqual(canJump([2,3,1,1,4]), True)
+        self.assertEqual(canJump([3,2,1,0,4]), False)
+        
+if __name__ == '__main__':
+    unittest.main()
